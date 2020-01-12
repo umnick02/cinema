5ffec69e899a9b4731a125b6b6d4f1dbdc3dc780
umnick02 <t.karen.bro@gmail.com>
2020-01-12 06:20:43 +0300

    remove spring

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -1,19 +1,9 @@
 package com.cinema;
 
 import javafx.application.Application;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
-public class Main implements CommandLineRunner {
-
+public class Main {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Main.class).headless(false).run(args);
-    }
-
-    @Override
-    public void run(String... args) {
         Application.launch(CinemaApplication.class);
     }
 }
